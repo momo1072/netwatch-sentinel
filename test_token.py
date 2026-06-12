f69316bf4424d427720fe94fc6d9645b9bfd936f
unknown <mohammadkhaknejhad@gfn.education>
2026-06-12 15:22:12 +0200

    chore: test datei entfernt

--- test_token.py
+++
@@ -1,9 +0,0 @@
-import requests
-from pathlib import Path
-
-for l in Path(".env").read_text().splitlines():
-    if "TELEGRAM_TOKEN" in l:
-        token = l.split("=", 1)[1].strip()
-
-r = requests.get(f"https://api.telegram.org/bot{token}/getMe")
-print(r.text)
